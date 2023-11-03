23b97d7df4abf932712e8ecabee7501cedb586b1
@drowkid01 <drowkid01@gmail.com>
2023-11-03 06:36:33 -0600

    bGFsaXRvZGsK

--- installbot.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-negro="\e[1;30m" && azul="\e[1;34m" && verde="\e[1;32m" && cian="\e[1;36m" && rojo="\e[1;31m" && purpura="\e[1;35m" && amarillo="\e[1;33m" && blanco="\e[1;37m" && sp="${verde}[✓]" && sx="${rojo}[✗]" && own="\e[1;32m＠\e[1;33mｄ\e[1;35mｒ\e[1;36mｏ\e[1;31mｗ\e[1;32mｋ\e[1;35mｉ\e[1;31mｄ\e[1;33m０\e[1;36m１\e[0m" && bar="${cian}════════════════════════════════════"
-init(){
-wget https://raw.githubusercontent.com/hostingadvanced/botdk/main/installbot.bash
-}
